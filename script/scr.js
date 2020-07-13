8098539393a56f65ac00a542b54d4d63244abca5
liezenberg <qikraytonip@gmail.com>
2020-07-14 01:41:46 +0300

    v.1.1.0

--- script/scr.js
+++ script/scr.js
@@ -1,37 +1,64 @@
 $(document).ready( function(){
-   //число получается когда кликаем по функциональной кнопке
-    let firstnumber;
-    let secondnumber;
-    let inputstr="";    
-      
-     //button click
-     $('button.num , button.functional').on('click', function(){  
-        inputstr+=$(this).attr('name');
-        $('input').val(inputstr);
-     });
-
-     function clear(){
+   let result;
+   let firstnumber;
+   let secondnumber;
+   let inputstr="";
+   const minusstr = $('button#minus').attr('id');    
+   const c = $('button#c').attr('id');  
+   const testRegex = /[0-9]/; 
+   const operators = /[^-][^0-9]$/ //если в строке после чисел есть знак выключаем функциональные клавиши
+  //Если строка пустая выключаем все кроме цифр и -
+   if (isEmpty(inputstr)) {
+      $("button.functional").addClass("disabled").prop("disabled", true);  
+      if(minusstr=='minus'|| c =='c'){
+      $("button#minus , button#c").removeClass("disabled").prop("disabled", false);
+      }
+   }
+   $('button.functional, button.num').on('click', function(){  
+      //Если не пустая включам функциональные клавиши
+      inputstr+=$(this).attr('name');
+      $('input').val(inputstr);
+      if(!isEmpty(inputstr)&&operators.test(inputstr)){
+         $("button.functional").addClass("disabled").prop("disabled", true);
+      }
+      else if(testRegex.test(inputstr)){
+         $("button.functional").removeClass("disabled").prop("disabled", false);
+      }
+   });
+  
+   
+   function clear(){
       $('input').val("");
       inputstr="";
-     }
-
-     $('button#c').on('click', clear);
-     
+      $("button.functional").addClass("disabled").prop("disabled", true);  
+      if(minusstr=='minus'|| c =='c'){
+      $("button#minus , button#c").removeClass("disabled").prop("disabled", false);
+      }
+   };
+   $('button#c').on('click', clear);
+   $('button.backspace').on('click',function(){
+         temp = inputstr.substring(0, inputstr.length - 1);
+         inputstr = temp;
+         $('input').val(inputstr);
+   });
       
-      $('button#backspace').on('click',function(){
-         var temp = inputstr.substring(0, inputstr.length - 1);
-         $('input').val(temp);
-        });
-      
-      $('button.result').on('click', function(){
-
-         try {
+   $('button.result').on('click', function(){
+      try{
             var result = eval(inputstr);
             $('input').val(result);
+            inputstr = result.toString();
+            
          } catch (error) {
-            alert("Error!")
+            $('input').val('Error!');
+            inputstr="";
          }
-         inputstr="";
+         
       });
-    
+   function isEmpty(str) {
+         if (str.trim() == '') 
+           return true;
+           
+         return false;
+   };
+       
 });
